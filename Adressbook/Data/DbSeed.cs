3163af6aac7dc4bb0a11f3a530903b659bb71f70
Håkan Hallberg <hakan.hallberg.spo16@ecusolna.onmicrosoft.com>
2017-12-22 10:52:05 +0100

    Made a Seed-class and seeded some people and adresses

---
+++ Adressbook/Data/DbSeed.cs
@@ -0,0 +1,29 @@
+﻿using Adressbook.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Adressbook.Data
+{
+    public class DbSeed
+    {
+        public static void Seed(ApplicationDbContext context)
+        {
+            var John = new Person { FirstName = "John", LastName = "Smith" };
+            context.People.Add(John);
+            var Sara = new Person { FirstName = "Sara", LastName = "Johnson" };
+            context.People.Add(Sara);
+            var Mickey = new Person { FirstName = "Mickey", LastName = "Mouse" };
+            context.People.Add(Mickey);
+            context.SaveChanges();
+
+            var Adress1 = new Adress { AdressName = "Namngatan 12" };
+            context.Adresses.Add(Adress1);
+            var Vägen2 = new Adress { AdressName = "Gatvägen 5" };
+            context.Adresses.Add(Vägen2);
+            var Gatan = new Adress { AdressName = "Väggatan 13" };
+            context.Adresses.Add(Gatan);
+        }
+    }
+}
