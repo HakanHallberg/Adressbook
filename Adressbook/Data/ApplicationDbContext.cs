3f2593707ad78082c065e2606a12ad5a88b4ba8e
Håkan Hallberg <hakan.hallberg.spo16@ecusolna.onmicrosoft.com>
2017-12-22 10:05:59 +0100

    Added model Person and it's DbSet<>

--- Adressbook/Data/ApplicationDbContext.cs
+++ Adressbook/Data/ApplicationDbContext.cs
@@ -13,6 +13,7 @@ namespace Adressbook.Data
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
         {
+
         }
 
         protected override void OnModelCreating(ModelBuilder builder)
@@ -22,5 +23,6 @@ namespace Adressbook.Data
             // For example, you can rename the ASP.NET Identity table names and more.
             // Add your customizations after calling base.OnModelCreating(builder);
         }
+        public DbSet<Person> People { get; set; }
     }
 }
--- Adressbook/Models/person.cs
+++ Adressbook/Models/person.cs
@@ -7,5 +7,9 @@ namespace Adressbook.Models
 {
     public class Person
     {
+        public int PersonID { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        
     }
 }
